8409a0d3e15e3bc77ad50957141fde58eac11faf
Andrei <andrei.romila@gmail.com>
2022-06-24 18:14:07 +0200

    Implementation of findById(Long id) method with 2 try-with-resources

--- src/main/java/com/andreiromila/dataaccess/App.java
+++ src/main/java/com/andreiromila/dataaccess/App.java
@@ -5,6 +5,7 @@ import com.andreiromila.dataaccess.repository.BookDao;
 import com.andreiromila.dataaccess.repository.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 public class App {
     public static void main(String[] args) {
@@ -15,6 +16,15 @@ public class App {
         books.forEach(book -> {
             System.out.println("Id: " + book.getId());
             System.out.println("Title: " + book.getTitle());
+        });
+
+        System.out.println("\n\n");
+
+        Optional<Book> firstBook = repository.findById(2L);
+        firstBook.ifPresent(book -> {
+            System.out.println("The first book is:");
+            System.out.println("\t Id: " + book.getId());
+            System.out.println("\t Title: " + book.getTitle());
         });
     }
 }
--- src/main/java/com/andreiromila/dataaccess/repository/BookDao.java
+++ src/main/java/com/andreiromila/dataaccess/repository/BookDao.java
@@ -2,10 +2,7 @@ package com.andreiromila.dataaccess.repository;
 
 import com.andreiromila.dataaccess.model.Book;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -48,6 +45,35 @@ public class BookDao extends AbstractDao implements Repository<Book, Long> {
 
     @Override
     public Optional<Book> findById(Long id) {
-        return Optional.empty();
+
+        final String sql = "SELECT id, title FROM book WHERE id = ?";
+
+        try (
+                Connection connection = getConnection();
+                PreparedStatement statement = connection.prepareStatement(sql);
+        ) {
+            // Add the query parameters
+            statement.setLong(1, id);
+
+            // We need a second try-with-resources because of the setLong method above
+            try (ResultSet results = statement.executeQuery()) {
+
+                if (results.next()) {
+                    Book book = new Book();
+
+                    book.setId(results.getLong("id"));
+                    book.setTitle(results.getString("title"));
+
+                    return Optional.of(book);
+                }
+
+                return Optional.empty();
+            }
+
+        } catch (SQLException sqle) {
+            sqle.printStackTrace();
+            throw new RuntimeException(sqle.getMessage(), sqle);
+        }
+
     }
 }
